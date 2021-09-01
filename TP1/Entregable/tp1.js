351ca3ba89b1ada5b03b7e9bdb3b8f1298a601b6
Diego Lopez <lopezdiegomartin5@gmail.com>
2021-09-01 17:00:49 -0300

    Agrego ejercio 1 TP1

---
+++ TP1/Entregable/tp1.js
@@ -0,0 +1,69 @@
+"use strict";
+
+let matriz = [];
+
+for(let x = 0; x < 100; x++){
+    matriz[x] = [];
+    for(let y = 0; y < 100; y++){
+        matriz[x][y] = Math.floor(Math.random()*500);
+    }
+}
+
+//Ejercicio 1) a
+function valorMaxMatriz(matriz){
+    let valorMayor = 0;
+    for(let x = 0; x < 100; x++){
+        for(let y = 0; y < 100; y++){
+            if(valorMayor < matriz[x][y]){
+                valorMayor = matriz[x][y];
+            }
+        }
+    }
+    return valorMayor;
+}
+
+//Ejercicio 1) b
+function valorMayorFilasPares(matriz){
+    let valoresMayores = [];
+    for(let x = 0; x < 100; x++){
+        let valorMayor = 0;
+        for(let y = 0; y < 100; y++){
+            if((x/2 == 0) && (valorMayor < matriz[x][y])){
+                valorMayor = matriz[x][y];
+
+            }
+        }
+        valoresMayores.push(valorMayor);
+    }
+    return valoresMayores;
+}
+
+function valorMenorFilasImpares(matriz){
+    let valoresMenores = [];
+    for(let x = 0; x < 100; x++){
+        let valorMenor = 500; //500 es el valor maximo que devuelve el math.ramdom
+        for(let y = 0; y < 100; y++){
+            if((x/2 != 0) && (valorMenor > matriz[x][y])){
+                valorMenor = matriz[x][y];
+            }
+        }
+        valoresMenores.push(valorMenor);
+    }
+    return valoresMenores;
+}
+
+//Ejercicio 1) c
+function promedioFila(matriz){
+    let promediosFila = [];
+    for(let x = 0; x < 100; x++){
+        let suma = 0;
+        let promedio = 0;
+        for(let y = 0; y < 100; y++){
+            suma = suma+matriz[x][y];
+        }
+        promedio = suma/100;
+        promediosFila.push(suma);
+    }
+    return promediosFila;
+}
+
