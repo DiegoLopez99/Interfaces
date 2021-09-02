49b3954252e83a7cb37c7ff2ffb86661e5a8350a
Diego Lopez <lopezdiegomartin5@gmail.com>
2021-09-02 17:45:18 -0300

    Corrijo errores

--- TP1/Practica/tp1.js
+++ TP1/Practica/tp1.js
@@ -68,4 +68,94 @@ function promedioFila(matriz){
 }
 
 
-//Ejercicio 2)
+//Ejercicio 3)
+
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+let width = canvas.width;
+let height = canvas.height;
+let imageData = ctx.createImageData(width, height);
+
+let r = 255;
+let g = 100;
+let b = 50;
+let a = 255;
+
+function drawRect(imageData, r, g, b, a){
+    for(let x = 0; x < width; x++){
+        for(let y = 0; y < height; y++){
+            setPixel(imageData, x, y, r, g, b, a);
+        }
+    }
+}
+
+function setPixel(imageData, x, y, r, g, b, a){
+    let index = (x + y * imageData.width) * 4;
+    imageData.data[index + 0] = r;
+    imageData.data[index + 1] = g;
+    imageData.data[index + 2] = b;
+    imageData.data[index + 3] = a;
+}
+
+drawRect(imageData, r, g, b, a);
+ctx.putImageData(imageData, 0, 0);
+
+//Ejercicio 4)
+
+let canvasDegrade = document.querySelector('#canvasDegrade');
+let ctx2 = canvasDegrade.getContext('2d');
+let canvasWidth = canvasDegrade.width;
+let canvasHeight = canvasDegrade.height;
+let imageData2 = ctx2.createImageData(canvasWidth, canvasHeight);
+
+function drawRectDegrade(imageData){
+    for(let x = 0; x < width; x++){
+        
+        for(let y = 0; y < height; y++){
+            var coeficiente = 255/height;
+            let r = coeficiente * y;
+            let g = coeficiente * y;
+            let b = coeficiente * y;
+            let a = 255
+            setPixel(imageData, x, y, r, g, b, a);
+        }
+    }
+}
+
+drawRectDegrade(imageData2);
+ctx2.putImageData(imageData2, 0, 0);
+
+
+//Ejercicio 5)
+
+let canvas5 = document.querySelector('#canvas5');
+let ctx5 = canvas5.getContext('2d');
+let width5 = canvas5.width;
+let height5 = canvas5.height;
+let imageData5 = ctx5.createImageData(width5, height5);
+
+function drawRect5(imageData){
+    for(let x = 0; x < width; x++){
+        let r;
+        let g ;
+        let b;
+        let a = 255
+        if(x <= width5/2){
+            var coeficiente = 255 / (width5/2);
+            r = coeficiente * x;
+            g = coeficiente * x;
+            b = 0;
+        }else{
+            var coeficiente2 = 255 / (width5);
+            r = coeficiente2 * x;
+            g = 255 - (coeficiente2 * x);
+            b = 0;
+        }
+        for(let y = 0; y < height5; y++){
+            setPixel(imageData, x, y, r, g, b, a);
+        }
+    }
+}
+
+drawRect5(imageData5);
+ctx5.putImageData(imageData5, 0, 0);
